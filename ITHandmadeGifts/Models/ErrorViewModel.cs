e2a4f7130eff9a69b987c90d1718a759e631f0e3
IvanaTrpeska <85795369+IvanaTrpeska@users.noreply.github.com>
2022-09-23 16:30:48 +0200

    Add project files.

---
+++ ITHandmadeGifts/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ITHandmadeGifts.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
